122c49071db5db5aa88ab3d64de2395b8c95e589
levoncii <levoncii23@gmail.com>
2021-10-14 13:13:00 +0300

    create login locators,page,test

--- pages/locators.py
+++ pages/locators.py
@@ -6,8 +6,6 @@ class MainPageLocators():
 
 
 class LoginPageLocators():
-    LOGIN_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
-    LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
-    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
-    REGISTER_PASSWORD1 = (By.CSS_SELECTOR, "#id_registration-password1")
-    REGISTER_PASSWORD2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+  
